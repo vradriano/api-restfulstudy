e45a4ace9763338a76d54d765d7e26d9a4d80c98
Vitor Adriano <vradriano@hotmail.com>
2021-11-02 14:08:07 -0300

    adicionando middleware de cors

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,7 +11,7 @@ const ProductController = require('../controllers/products')
 router.get('/products/:id?', ProductController.get)
 router.post('/products', ProductController.post)
 router.put('/products/:id', ProductController.put)
-router.delete('/products/:id', ProductController.remove)
+router.delete('/produtos/:id', ProductController.remove)
 
  
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 
@@ -6,6 +7,24 @@ const db = require('./database/db')
 const routes = require('./routes/routes')
 
 db.connect()
+
+const allowedOrigins = [
+  'http://192.168.43.100:5500',
+  'http://www.google.com.br',
+]
+
+app.use(cors({
+  origin: function(origin, callback){
+    let allowed = true
+
+    if (!origin) allowed = true
+
+    if (!allowedOrigins.includes(origin)) allowed = false
+
+    callback(null, allowed)
+  }
+}))
+
 
 //habilita server para receber dados json
 app.use(express.json())
